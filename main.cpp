64eb5342b69290b7445e37341a7ccf7f8a17a8a3
Brian022 <38355408+Brian022@users.noreply.github.com>
2019-06-12 03:52:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,29 +25,35 @@ class Lista
     Node<T> *head;
     Node<T> *actual;
 
+    //Añade un nuevo nodo al final de la  lista 
     void add(T data)
     {
+        //Si el nodo inicial(head) de la lista no contiene nada, lo inicializa con el contenido de data
         if(head == NULL)
         {
             head = new Node<T>(data);
             return;
         }
+        //actual un puntero a nodo se iguala a head, haciendo que recorra la lista hasta que encuentre el nodo final y agregar el nuevo dato
         Node<T> *actual = head;
         while(actual->sgte != NULL){actual = actual->sgte;}
         actual->sgte = new Node<T>(data);
     }
 
-
+    // Similar a la logica de add con la diferencia que aqui el elemento data se agrega como la cabeza es decir el primer elemento de  la lista
     void addF(T data)
     {
         Node<T> *newHead = new Node<T>(data);
         newHead->sgte = head;
         head = newHead;
     }
-
+    
+    //Funcion para borrar un elemento de la lista por un dato ingresado
     void deleteV(T x)
     {
-        if(head = NULL) return;
+        
+        if(head = NULL) return;//comprueba si la lista esta vacia, si es el caso no tiene nada que hacer y sale de la funciòn
+        //Si encuentra el dato a borrar en la cabeza de la lista utiliza un nodo temporal para hacer el cambio al sgte nodo como cabeza y libera memoria
         if(head->data == x)
         {
             Node<T> *temp = head;
@@ -55,7 +61,8 @@ class Lista
             delete temp;
             return;
         }
-
+        
+        //Recorre todos los nodos buscando el valor a borrar y al nodo anterior al nodo borrado lo  hace apuntar hacia el sgte nodo del nodo borrado 
         Node<T> *actual = head;
         while(actual->sgte != NULL)
         {
@@ -67,10 +74,13 @@ class Lista
             actual = actual->sgte;
         }
     }
-
+    
+    //funciòn para imprimir la lista 
     void imprimir()
     {
+        //se crea un nodo temp para que luego recorra la lista mientras que el nodo no sea NULL, luego usando la funcion imprimirN de Nodo imprime los valores
         Node<T> *temp = head;
+        
         if (!head) {
             cout << "Lista no inicializada " << endl;
         } else {
